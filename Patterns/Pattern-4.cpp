d126397205086e3fecacc54a645c350b08717a05
sawantmrunali115 <sawantmrunali115@gmail.com>
2026-03-04 17:39:38 +0530

    Pattern 4 Done

---
+++ Patterns/Pattern-4.cpp
@@ -0,0 +1,43 @@
+/*
+Problem: matrix of numbers
+Platform: NA
+Difficulty: Easy
+Concept: NA
+Time Complexity: O(n)
+Space Complexity: O(n)
+Date Solved: 04-03-2026
+*/
+
+/*
+Output: 
+1 
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cout << "Enter number of test cases: ";
+    cin >> t;
+
+    for(int i=0; i<t; i++) {
+        int n;
+        cin >> n;
+        print(n);
+    }
+}
