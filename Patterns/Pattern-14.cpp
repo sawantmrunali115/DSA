c76e2d98a42b777bc0dfac817ea7deb2a6e7e0b1
sawantmrunali115 <sawantmrunali115@gmail.com>
2026-09-03 23:36:21 +0530

    Pattern 14 Done

---
+++ Patterns/Pattern-14.cpp
@@ -0,0 +1,44 @@
+/*
+Problem: matrix of alphabets
+Platform: NA
+Difficulty: Easy
+Concept: NA
+Time Complexity: O(n)
+Space Complexity: O(n)
+Date Solved: 03-09-2026
+*/
+
+/*
+Output: 
+A
+A B 
+A B C
+A B C D
+A B C D E
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+    int num = 1;
+    for (int i=1; i<=n; i++){
+        for(char ch='A'; ch<'A'+i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+int main() {
+    int t; 
+    cout << "Enter number of test cases: ";
+    cin >> t;
+
+    for(int i=0; i<t; i++) {
+        int n;
+        cin >> n;
+        print(n);
+    }
+}
